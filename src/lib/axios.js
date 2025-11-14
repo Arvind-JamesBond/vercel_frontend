ccebb2fed1b8d4e26f7ede024bbbeeb91cac84a0
Arvind Kumar <arvindedla9905407353@gmail.com>
2025-11-14 20:10:29 +0530

    fourh

--- src/lib/axios.js
+++ src/lib/axios.js
@@ -1,6 +1,6 @@
 import axios from "axios";
 
 export const axiosInstance = axios.create({
-  baseURL: import.meta.env.MODE === "development" ? "https://vercel-frontend-njko.onrender.com/api": "/api",
+  baseURL: import.meta.env.MODE === "development" ? "https://vercel-frontend-njko.onrender.com": "/api",
   withCredentials: true,
 });
